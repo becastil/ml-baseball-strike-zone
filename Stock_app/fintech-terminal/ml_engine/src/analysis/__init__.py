077e99a608af21b3bd46d5a0a6ca720d135ce777
becastil <bencastillo14@yahoo.com>
2025-06-20 11:25:08 -0700

    Delete Stock_app directory

--- Stock_app/fintech-terminal/ml_engine/src/analysis/__init__.py
+++
@@ -1,4 +0,0 @@
-from .technical_analysis import TechnicalAnalysis
-from .sentiment_analyzer import SentimentAnalyzer
-
-__all__ = ["TechnicalAnalysis", "SentimentAnalyzer"]
--- Stock_app/fintech-terminal/ml_engine/src/predictors/__init__.py
+++
@@ -1,4 +0,0 @@
-from .price_predictor import PricePredictor
-from .trend_classifier import TrendClassifier
-
-__all__ = ["PricePredictor", "TrendClassifier"]
